42861c04a097c5f291c2a204ab08d9d7b6eaef87
karin0 <karin0@gmx.com>
2021-11-26 02:13:41 +0800

    layout: provide offset prop for AppLayout

--- src/routes/form/FormCreate.jsx
+++ src/routes/form/FormCreate.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Grid, Header, Icon, Input, Label, Segment, Sticky, Transition } from 'semantic-ui-react';
+import { Button, Grid, Header, Icon, Input, Label, Segment, Sticky, Transition } from 'semantic-ui-react';
 
 import AppLayout from 'layouts/AppLayout';
 import api from 'api';
@@ -64,56 +64,54 @@ function FormCreate() {
   }
 
   return (
-    <AppLayout>
-      <Container style={{marginTop: '5.5em'}}>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={4}>
-              <Sticky offset={70}>
-                <Segment>
-                  <Header>添加题目</Header>
-                  {qTypes.map(l => (
-                    <Label key={l[0]} as='a' onClick={() => addQuestion(l[0])} style={{'margin': 3}}>
-                      <Icon name={l[2]} />
-                      {l[1]}
-                    </Label>
-                  ))}
-                </Segment>
-              </Sticky>
-            </Grid.Column>
-            <Grid.Column width={12}>
-              <Input
-                error={titleError}
-                placeholder='问卷标题'
-                value={title}
-                onChange={e => {
-                  setTitleError(false);
-                  const nv = e.target.value;
-                  if (nv.length <= 200)
-                    setTitle(nv);
-                }}
-              />
-              <Button
-                primary floated='right'
-                onClick={onSubmit}
-              >
-                创建问卷
-              </Button>
-              <Transition.Group duration={180}>
-                {qids.map(qid => {
-                  const E = editorMap[qMap[qid].type];
-                  return <Segment key={qid}>
-                    <Input placeholder='问题' onChange={e => {
-                      qMap[qid].title = e.target.value;
-                    }} />
-                    <E qid={qid} ctx={ctx} />
-                  </Segment>;
-                })}
-              </Transition.Group>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+    <AppLayout offset>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Sticky offset={70}>
+              <Segment>
+                <Header>添加题目</Header>
+                {qTypes.map(l => (
+                  <Label key={l[0]} as='a' onClick={() => addQuestion(l[0])} style={{'margin': 3}}>
+                    <Icon name={l[2]} />
+                    {l[1]}
+                  </Label>
+                ))}
+              </Segment>
+            </Sticky>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Input
+              error={titleError}
+              placeholder='问卷标题'
+              value={title}
+              onChange={e => {
+                setTitleError(false);
+                const nv = e.target.value;
+                if (nv.length <= 200)
+                  setTitle(nv);
+              }}
+            />
+            <Button
+              primary floated='right'
+              onClick={onSubmit}
+            >
+              创建问卷
+            </Button>
+            <Transition.Group duration={180}>
+              {qids.map(qid => {
+                const E = editorMap[qMap[qid].type];
+                return <Segment key={qid}>
+                  <Input placeholder='问题' onChange={e => {
+                    qMap[qid].title = e.target.value;
+                  }} />
+                  <E qid={qid} ctx={ctx} />
+                </Segment>;
+              })}
+            </Transition.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </AppLayout>
   );
 }
--- src/routes/form/FormSet.jsx
+++ src/routes/form/FormSet.jsx
@@ -1,18 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Button,
-  Card,
-  Container,
-  Dropdown,
-  Form,
-  Grid,
-  Icon,
-  Input,
-  Label,
-  Menu,
-  Message,
-  Transition
-} from 'semantic-ui-react';
+import { Button, Card, Dropdown, Form, Grid, Icon, Input, Label, Menu, Message, Transition } from 'semantic-ui-react';
 
 import AppLayout from 'layouts/AppLayout';
 import api from 'api';
@@ -133,68 +120,66 @@ function FormSet() {
   );
 
   return (
-    <AppLayout>
-      <Container style={{marginTop: '5.5em'}}>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Menu vertical pointing>
-                <Menu.Item active>
-                  <Label>1</Label>
-                  <code>{'TODO: folders'}</code>
-                </Menu.Item>
-                <Menu.Item>
-                  <Label>51</Label>
-                  Folder 2
-                </Menu.Item>
-                <Menu.Item>
-                  <Label>1</Label>
-                  Folder 3
-                </Menu.Item>
-              </Menu>
-            </Grid.Column>
-            <Grid.Column width={1} />
-            <Grid.Column width={12}>
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Menu pagination>
-                      <Menu.Item as='a' icon>
-                        <Icon name='chevron left' />
-                      </Menu.Item>
-                      <Menu.Item as='a'><code>{'TODO: pagination'}</code></Menu.Item>
-                      <Menu.Item as='a'>2</Menu.Item>
-                      <Menu.Item as='a'>3</Menu.Item>
-                      <Menu.Item as='a'>4</Menu.Item>
-                      <Menu.Item as='a' icon>
-                        <Icon name='chevron right' />
-                      </Menu.Item>
-                    </Menu>
-                    <Button
-                      primary size='large' floated='right'
-                      href='/form/create'
-                    >
-                      创建问卷
-                    </Button>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    {sharingMessage}
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Card.Group itemsPerRow={3}>
-                      {cards}
-                    </Card.Group>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+    <AppLayout offset>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <Menu vertical pointing>
+              <Menu.Item active>
+                <Label>1</Label>
+                <code>{'TODO: folders'}</code>
+              </Menu.Item>
+              <Menu.Item>
+                <Label>51</Label>
+                Folder 2
+              </Menu.Item>
+              <Menu.Item>
+                <Label>1</Label>
+                Folder 3
+              </Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={12}>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                  <Menu pagination>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron left' />
+                    </Menu.Item>
+                    <Menu.Item as='a'><code>{'TODO: pagination'}</code></Menu.Item>
+                    <Menu.Item as='a'>2</Menu.Item>
+                    <Menu.Item as='a'>3</Menu.Item>
+                    <Menu.Item as='a'>4</Menu.Item>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron right' />
+                    </Menu.Item>
+                  </Menu>
+                  <Button
+                    primary size='large' floated='right'
+                    href='/form/create'
+                  >
+                    创建问卷
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  {sharingMessage}
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Card.Group itemsPerRow={3}>
+                    {cards}
+                  </Card.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </AppLayout>
   );
 }
