d810469a0f5bed263df0715190405cf9c2f13c15
karin0 <karin0@gmx.com>
2021-11-26 16:12:57 +0800

    user: profile: make username an Input and fix form error

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import { Form, Message } from 'semantic-ui-react';
 
 import api from 'api';
 import appState from 'appState';
-import { checkDname, checkEmail, useField } from 'utils';
 import ProfileLayout from 'layouts/ProfileLayout';
-import { Form, Message } from 'semantic-ui-react';
+import { checkDname, checkEmail, useField, validateFields } from 'utils';
 
 function Profile() {
   const currentInfo = appState.user_info;
@@ -15,7 +15,7 @@ function Profile() {
   const [errorPrompt, setErrorPrompt] = useState(null);
 
   async function onSubmit() {
-    if (!emailField.validate())
+    if (!validateFields(emailField, dnameField))
       return;
     let res;
     try {
@@ -24,40 +24,39 @@ function Profile() {
       return setErrorPrompt(e.toString());
     }
     if (res.code === 0)
-      window.location = '/profile';
+      window.location.reload();
     else if (res.code === api.ERR_DUPL_EMAIL)
       setErrorPrompt('该邮箱已注册');
     else
       setErrorPrompt(res.code);
   }
 
-
   return (
-    <ProfileLayout page={'profile'}>
-      <Form>
-        <Form.Field
-          label={'用户名'}
-        />
-        <Message>
-          <p>{currentInfo.username}</p>
-        </Message>
+    <ProfileLayout page='profile'>
+      <Form error={errorPrompt !== null}>
         <Form.Input
-          label={'邮箱'}
+          label='用户名'
+          value={currentInfo.username}
+        />
+        <Form.Input
+          label='邮箱'
           {...emailField.toProps()}
         />
         <Form.Input
-          label={'显示名称'}
+          label='显示名称'
           {...dnameField.toProps()}
         />
-        <Message error header='修改失败' content={errorPrompt} />
+        <Message error header='保存失败' content={errorPrompt} />
         <Form.Button
           primary fluid
           onClick={onSubmit}
-          disabled={!(emailField.visuallyValid() && dnameField.visuallyValid()) ||
-          (currentInfo.email === emailField.value && currentInfo.dname === dnameField.value)}
+          disabled={
+            !(emailField.visuallyValid() && dnameField.visuallyValid()) ||
+            (currentInfo.email === emailField.value && currentInfo.dname === dnameField.value)
+          }
           className='profile-submit'
         >
-          保存修改
+          保存
         </Form.Button>
       </Form>
     </ProfileLayout>
