1ba6d14452f89dc2e3b22347306c671d5fa03c67
karin0 <karin0@gmx.com>
2021-11-29 02:09:20 +0800

    form: settings: add todos

--- src/routes/form/detail/FormSettings.jsx
+++ src/routes/form/detail/FormSettings.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import DetailLayout from './DetailLayout';
+import { Form } from 'semantic-ui-react';
 
 function FormSettings() {
   const fid = parseInt(useParams().fid, 10);
 
+  // TODO
   return (
     <DetailLayout offset='settings' fid={fid}>
-      TODO
+      <Form>
+        <Form.Checkbox label='接受新答卷' />
+        <Form.Group>
+          <Form.Input type='datetime-local' label='该时间后开始接受答卷' />
+          <Form.Input type='datetime-local' label='该时间后停止接受答卷' />
+        </Form.Group>
+
+        <Form.Input label='访问密码' />
+        <Form.Checkbox label='仅登录用户可见' />
+        <Form.Checkbox label='仅组织内用户可见' />
+      </Form>
     </DetailLayout>
   );
 }
