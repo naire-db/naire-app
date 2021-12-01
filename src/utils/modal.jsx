eceb889e61530ed5ea3de219a25603e20da4ec72
karin0 <karin0@gmx.com>
2021-12-01 12:44:08 +0800

    utils: modal: impl closeOnDimmerClick

--- src/utils/modal.jsx
+++ src/utils/modal.jsx
@@ -1,8 +1,9 @@
-import { Button, Form, Header, Modal, TransitionablePortal } from 'semantic-ui-react';
+import { Button, Form, Header, Modal, Ref, TransitionablePortal } from 'semantic-ui-react';
 import { action, makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react-lite';
 
 import { resolvePossibleAction } from './index';
+import { useEffect, useRef } from 'react';
 
 const modalState = makeAutoObservable({
   title: 'Title',
@@ -30,63 +31,93 @@ const modalState = makeAutoObservable({
   })
 });
 
-const CommonModal = observer(() =>
-  (
-    <TransitionablePortal
-      open={modalState.open}
-      transition={{
-        animation: 'scale',
-        duration: 150
-      }}
-    >
-      <Modal
-        open={true}
-        size={modalState.size}
-        onClose={modalState.onClosed}
+const CommonModal = observer(() => {
+  // closeOnDimmerClick is somehow broken, so we do it on our own
+  const ref = useRef();
+  useEffect(() => {
+    const {current: dim} = ref;
+    if (dim) {
+      const modal = dim.querySelector('.modal');
+      if (modal) {
+        const vis = new Map();
+
+        function dimHandler(e) {
+          if (!vis.get(e)) {
+            modalState.onCancelled();
+          }
+        }
+
+        function modalHandler(e) {
+          vis.set(e, true);
+        }
+
+        dim.addEventListener('click', dimHandler);
+        modal.addEventListener('click', modalHandler);
+        return () => {
+          dim.removeEventListener('click', dimHandler);
+          modal.removeEventListener('click', modalHandler);
+        };
+      }
+    }
+  });
+  return (
+    <Ref innerRef={ref}>
+      <TransitionablePortal
+        open={modalState.open}
+        transition={{
+          animation: 'scale',
+          duration: 150
+        }}
       >
-        <Header>
-          {modalState.title}
-          {modalState.subtitle &&
-            <Header.Subheader>
-              {modalState.subtitle}
-            </Header.Subheader>
-          }
-        </Header>
-        <Modal.Content>
-          {modalState.description &&
-            <Modal.Description>
-              {modalState.description}
-            </Modal.Description>
-          }
-          {modalState.content && resolvePossibleAction(modalState.content, modalState.state)}
-          {modalState.inputProps && (
-            <Form className='modal-input'>
-              <Form.Input
-                {...modalState.inputProps}
-              />
-            </Form>
-          )}
-        </Modal.Content>
-        <Modal.Actions>
-          <Button
-            content='取消'
-            onClick={modalState.onCancelled}
-          />
-          <Button
-            primary
-            content={modalState.confirmText}
-            onClick={modalState.onConfirmed}
-            {...(
-              modalState.confirmProps
-                ? resolvePossibleAction(modalState.confirmProps, modalState.state)
-                : {}
+        <Modal
+          open={true}
+          size={modalState.size}
+          onClose={modalState.onClosed}
+        >
+          <Header>
+            {modalState.title}
+            {modalState.subtitle &&
+              <Header.Subheader>
+                {modalState.subtitle}
+              </Header.Subheader>
+            }
+          </Header>
+          <Modal.Content>
+            {modalState.description &&
+              <Modal.Description>
+                {modalState.description}
+              </Modal.Description>
+            }
+            {modalState.content && resolvePossibleAction(modalState.content, modalState.state)}
+            {modalState.inputProps && (
+              <Form className='modal-input'>
+                <Form.Input
+                  {...modalState.inputProps}
+                />
+              </Form>
             )}
-          />
-        </Modal.Actions>
-      </Modal>
-    </TransitionablePortal>
-  )
-);
+          </Modal.Content>
+          <Modal.Actions>
+            <Button
+              content='取消'
+              onClick={modalState.onCancelled}
+            />
+            <Button
+              primary
+              content={modalState.confirmText}
+              onClick={modalState.onConfirmed}
+              {...(
+                modalState.confirmProps
+                  ? resolvePossibleAction(modalState.confirmProps, modalState.state)
+                  : {}
+              )}
+            />
+          </Modal.Actions>
+        </Modal>
+      </TransitionablePortal>
+    </Ref>
+  );
+});
 
 function closeModal() {
   modalState.setOpen(false);
