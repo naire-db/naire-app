75750cf997c48026ad27d319d155f9fd134d188e
karin0 <karin0@gmx.com>
2021-12-02 12:04:46 +0800

    utils: paginate: make it a jsx

--- src/utils/paginate.jsx
+++ src/utils/paginate.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Pagination } from 'semantic-ui-react';
 
 function usePagination(items, {maxPageSize = 30} = {}) {
