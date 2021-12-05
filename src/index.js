3ce70632615c85db17f72470703a20cdde412f30
karin0 <karin0@gmx.com>
2021-12-06 00:51:08 +0800

    index: add error boundary

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 
-import './index.css';
 import AppRouter from './AppRouter';
+import ErrorFallback from './ErrorFallback';
+
+import './index.css';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {error: null};
+  }
+
+  static getDerivedStateFromError(error) {
+    return {error};
+  }
+
+  render() {
+    const {error} = this.state;
+    if (error)
+      return <ErrorFallback error={error} />;
+    return this.props.children;
+  }
+}
 
 ReactDOM.render(
-  <AppRouter />,
-  // <React.StrictMode>
-  // </React.StrictMode>,
+  <ErrorBoundary>
+    <AppRouter />
+  </ErrorBoundary>,
   document.getElementById('root')
 );
