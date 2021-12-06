bf85dc845cd308a384a63cea8483b8fe3c5cc8d9
karin0 <karin0@gmx.com>
2021-12-06 20:19:24 +0800

    util: render: adjust time rendering format

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,6 +1,15 @@
+import dayjs from 'dayjs';
+
+const now = dayjs();
+const now_year = now.year();
+
 function formatTimestamp(ts) {
-  const dt = new Date(ts * 1000);
-  return dt.toLocaleString();
+  // const dt = new Date(ts * 1000);
+  // return dt.toLocaleString();
+  const t = dayjs.unix(ts);
+  if (t.year() === now_year)
+    return t.format('MM-DD HH:mm:ss');
+  return t.format('YYYY-MM-DD HH:mm:ss');
 }
 
 function formatUser(user) {
