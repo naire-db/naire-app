54ee78eae29156b64f72a0926fffd3f9d55f6606
SprLau <lau@buaa.edu.cn>
2021-12-07 18:39:31 +0800

    dev-dropdown-view: finish dropdown view

--- src/routes/form/views/index.js
+++ src/routes/form/views/index.js
@@ -2,6 +2,10 @@ import { aMap, beforeSaveHookMap, initialMap, viewMap } from './base';
 import './checkbox';
 import './radio';
 import './input';
+<<<<<<< HEAD
 import './file';
+=======
+import './dropdown';
+>>>>>>> 053b909 (dropdown-view: finish)
 
 export { viewMap, aMap, initialMap, beforeSaveHookMap };
