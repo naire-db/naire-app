5bf8545dea27e0cc3ad4a14f28812a28cf0d1035
karin0 <karin0@gmx.com>
2021-12-08 00:27:01 +0800

    comps: NavButton: add as an animated button for navigation and so on

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+
+function NavButton(props) {
+  const {control: Control = Button, recoverable = false} = props;
+  const [loading, setLoading] = useState(false);
+
+  const onClick = recoverable ? async (...args) => {
+    setLoading(true);
+    await props.onClick?.(...args);
+    setLoading(false);
+  } : (...args) => {
+    setLoading(true);
+    props.onClick?.(...args);
+  };
+
+  return <Control
+    {...props}
+    control={undefined}
+    loading={loading}
+    disabled={loading}
+    onClick={onClick}
+  />;
+}
+
+function NavFormButton(props) {
+  return <NavButton
+    {...props}
+    control={Form.Button}
+  />;
+}
+
+NavButton.Form = NavFormButton;
+
+export default NavButton;
