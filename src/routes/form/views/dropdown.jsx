ef661d3597cf0fa4d19d45d5734c14a2e0f2141f
karin0 <karin0@gmx.com>
2021-12-07 23:21:20 +0800

    form: views: dropdown: fix missing error state

--- src/routes/form/views/dropdown.jsx
+++ src/routes/form/views/dropdown.jsx
@@ -7,27 +7,31 @@ function DropdownView(props) {
   const q = props.question;
   const [choice, setChoice] = useAState(q);
 
+  const [error, setError] = props.useErrorState(true);
+
   const downOptions = q.options.map(
     ({id, text}) =>
-      ({key: id, text, value: id}
-    ));
+      ({key: id, text, value: id})
+  );
 
   function onChange(_, d) {
     setChoice(d.value);
+    setError(false);
   }
 
-  return <Form>
-    {
+  return (
+    <Form>
       <Form.Dropdown
+        error={props.tried && error}
         fluid
         selection
-        placeholder='选择...'
+        placeholder='请选择'
         options={downOptions}
         value={choice}
         onChange={onChange}
       />
-    }
-  </Form>;
+    </Form>
+  );
 }
 
 registerQuestionView('dropdown', DropdownView, () => null);
