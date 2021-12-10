694086e198eb4a9f16f7fc121845994345ee5842
SprLau <lau@buaa.edu.cn>
2021-12-10 19:08:10 +0800

    dev-input-view-regex: finish regex views and resps

--- src/routes/form/views/input.jsx
+++ src/routes/form/views/input.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Form } from 'semantic-ui-react';
 
 import { registerQuestionView, useAState } from './base';
 import { QLabel } from './utils';
+import { regexMap } from '../RegexMap';
 
 function InputView(props) {
   const q = props.question;
   const [value, setValue] = useAState(q);
   const {min_length, max_length, regex} = q;
 
-  const [error, setError] = props.useErrorState(() => min_length > 0);
+  const [error, setError] = props.useErrorState(() => min_length > 0 || !matchRegex(''));
+
+  const reg = useMemo(() => {
+    return new RegExp(regex);
+  }, [regex]);
 
   function onChange(e) {
     const v = e.target.value;
     if (v.length > max_length)
       return;
-    setError(v.length < min_length);
+    setError(v.length < min_length || !matchRegex(v));
     setValue(v);
   }
 
-  // TODO: regex
+  function regexQLabel() {
+    if (regex === regexMap['id']) {
+      return '身份证号码';
+    } else if (regex === regexMap['telephone']) {
+      return '固定电话号码';
+    } else if (regex === regexMap['email']) {
+      return '电子邮箱';
+    } else if (regex === regexMap['mobile']) {
+      return '移动电话号码';
+    } else if (regex === regexMap['n']) {
+      return '';
+    } else return regex;
+  }
+
+  function matchRegex(value) {
+    return reg.test(value);
+  }
 
   return <Form>
     <Form.Input
@@ -39,6 +60,13 @@ function InputView(props) {
     <QLabel
       text={`最多输入 ${max_length} 个字符`}
     />
+    {
+      regex !== null && regexQLabel() !== '' &&
+      <QLabel
+        text={regexQLabel()}
+        error={props.tried && !matchRegex(value)}
+      />
+    }
   </Form>;
 }
 
