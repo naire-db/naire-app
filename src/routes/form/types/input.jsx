74b872a1a54691c532d5c64ecced9498b9a21365
SprLau <lau@buaa.edu.cn>
2021-12-10 19:08:10 +0800

    dev-input-view-regex: finish regex views and resps

--- src/routes/form/types/input.jsx
+++ src/routes/form/types/input.jsx
@@ -1,41 +1,13 @@
 import React from 'react';
-import { Dropdown, Form, Input } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 
 import NumberInput from 'components/NumberInput';
 import { BaseQuestion, registerQuestionType, useQState } from './base';
 import { makeRangeNumberInputProps, unwrap_nullable, wrap_nullable } from './utils';
+import { regexMap, regexTemplates } from '../RegexMap';
 
 const MAX_LENGTH = 200;
 
-const regexTemplates = [
-  {
-    key: 'mobile',
-    text: '移动电话号码（例如：13000000000）',
-    value: 'mobile',
-  },
-  {
-    key: 'email',
-    text: '电子邮箱（例如：user@domain.com）',
-    value: 'email',
-  },
-  {
-    key: 'id',
-    text: '身份证号码（15位或18位）',
-    value: 'id'
-  },
-  {
-    key: 'telephone',
-    text: '固定电话（例如：010-88888888 或 0511-8888888）',
-    value: 'telephone'
-  }
-];
-
-const regexMap = {
-  id: String.raw`(\d{18})|(\d{15})`,
-  telephone: String.raw`\d{3}-\d{8}|\d{4}-\d{7}`,
-  email: String.raw`(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))`,
-  mobile: String.raw`(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}`
-};
 
 class InputQuestion extends BaseQuestion {
   type = 'input';
@@ -92,23 +64,16 @@ function InputEditor(props) {
         />
       </Form.Group>
       {
-        !noRegex &&
-        <Form.Field>
-          <Input
-            value={regex}
-            onChange={e => setRegex(e.target.value)}
-            label='正则表达式'
-            placeholder='无正则匹配要求则留空'
-          />
-          <Dropdown
-            value={ddValue || null}
-            selection
-            options={regexTemplates}
-            onChange={handleSelection}
-            placeholder='常用正则表达式'
-          />
-        </Form.Field>
+        !noRegex && <Form.Dropdown
+          label={'输入格式限制'}
+          value={ddValue || null}
+          selection
+          options={regexTemplates}
+          onChange={handleSelection}
+          placeholder='常用格式限制'
+        />
       }
+
     </Form>
   );
 }
