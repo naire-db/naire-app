61da855742962d88d912573ab9a6abff0e29ab77
karin0 <karin0@gmx.com>
2021-12-10 19:38:10 +0800

    form: regex: formalize preset texts

--- src/routes/form/RegexMap.jsx
+++ src/routes/form/RegexMap.jsx
@@ -1,7 +1,7 @@
 const regexTemplates = [
   {
     key: 'mobile',
-    text: '移动电话号码（例如：13000000000）',
+    text: '手机号码（例如：13000000000）',
     value: 'mobile',
   },
   {
@@ -11,17 +11,17 @@ const regexTemplates = [
   },
   {
     key: 'id',
-    text: '身份证号码（15位或18位）',
+    text: '身份证号码（15 位或 18 位）',
     value: 'id'
   },
   {
     key: 'telephone',
-    text: '固定电话（例如：010-88888888 或 0511-8888888）',
+    text: '固定电话号码（例如：010-88888888 或 0511-8888888）',
     value: 'telephone'
   },
   {
     key: 'n',
-    text: '无',
+    text: '无限制',
     value: 'n'
   }
 ];
@@ -31,7 +31,7 @@ const regexMap = {
   telephone: String.raw`^((\d{3}-\d{8})|(\d{4}-\d{7}))$`,
   email: String.raw`^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$`,
   mobile: String.raw`^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$`,
-  n: String.raw`.*`
+  n: ''
 };
 
 export { regexMap, regexTemplates };
